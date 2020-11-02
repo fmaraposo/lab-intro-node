602040d3b86e1e828494453a35987b2ee037b582
Francisco Martins <fmaraposo@gmail.com>
2020-11-02 15:51:11 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,69 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
 
-  get(pos) {}
+    
+    this.items.sort((a,b) => {
+      return a-b;
+    });
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    for(let i=0; i<this.items.length; i++) {
+      if (pos <= this.items.length) {
+        return this.items[pos];
+      } else {
+        throw new Error('OutOfBounds');
+      }
+    }
+  }
 
-  min() {}
+  max() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.items.length-1];
+    }
+  }
 
-  sum() {}
+  min() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+  }
 
-  avg() {}
+  sum() {
+    return this.items.reduce((a,b) => a+b,0);
+  }
+
+  avg() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce((a,b) => a+b,0)/this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
+
+/* const sortedList = new SortedList();
+
+sortedList.add(9);
+
+console.log(sortedList);
+
+sortedList.add(8);
+
+console.log(sortedList);
+
+sortedList.add(1);
+console.log(sortedList); */
